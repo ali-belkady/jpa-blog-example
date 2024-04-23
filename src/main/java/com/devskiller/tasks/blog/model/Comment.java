5ba12d2882be43c2b154a354b04704b74525c07b
Ali BELKADY <dev.devagia@gmail.com>
2024-04-23 20:57:30 +0200

    fichier non pushé

---
+++ src/main/java/com/devskiller/tasks/blog/model/Comment.java
@@ -0,0 +1,60 @@
+package com.devskiller.tasks.blog.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Comment {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String author;
+
+	@Column(length = 4096)
+	private String content;
+
+	private LocalDateTime creationDate;
+
+	@ManyToOne
+	@JoinColumn(name = "post_id")
+	private Post post;
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public LocalDateTime getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(LocalDateTime creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Post getPost() {
+		return post;
+	}
+
+	public void setPost(Post post) {
+		this.post = post;
+	}
+}
